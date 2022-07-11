af1fa49811a64ee5887f28ecc590f0ab942656a5
Salambek <92019591+DerWaldgang@users.noreply.github.com>
2022-07-11 14:20:41 +0300

    Delete task_2 directory

--- task_2/array_2.js
+++
@@ -1,5 +0,0 @@
-let strArr = ['я', 'ты','он','она','оно','мы','вы','восемь','девять','десять']
-
-for(let i = 0; i < strArr.length; i++){
-    console.log(strArr[i])
-}
--- task_2/array_5.js
+++
@@ -1,8 +0,0 @@
-let names = ['Адам', 'Альви','Сулейман', 'Джабраил','Мовсар','Исмаил','Зелим']
-
-for(let i = 0; i < names.length; i++){
-    if(names[i].startsWith('А')) {
-        console.log(names[i])
-    }
-}
-
